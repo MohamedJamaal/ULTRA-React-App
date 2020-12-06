24c58f022e564024f8e54e1b8fba628bd0faa811
MohamedJamaal <mohamed_gamal19@hotmail.com>
2020-12-07 01:24:03 +0200

    Add Seperated Pages for App

--- src/App.js
+++ src/App.js
@@ -1,13 +1,26 @@
 import './App.css';
 import GlobalStyles from './GlobalStyles'
 import {BrowserRouter as Router, Switch, Route} from 'react-router-dom'
-import {Navbar} from './components';
+import {Footer, Navbar} from './components';
+import Home from './pages/HomePage/Home';
+import Services from './pages/Services/Services';
+import Products from './pages/Products/Products';
+import SignUp from './pages/Sign-Up/SignUp';
+import ScrollToTop from './components/ScrollToTop';
 
 function App() {
   return (
     <Router>
       <GlobalStyles />
+      <ScrollToTop />
       <Navbar />
+      <Switch>
+        <Route path="/" exact component={Home} />
+        <Route path="/services" component={Services} />
+        <Route path="/products" component={Products} />
+        <Route path="/sign-up" component={SignUp} />
+      </Switch>
+      <Footer />
     </Router>
   );
 }
---
+++ src/pages/Products/Data.js
@@ -0,0 +1,70 @@
+import home from '../../images/home.svg';
+import home2 from '../../images/home2.svg';
+import home3 from '../../images/home3.svg';
+import home4 from '../../images/home4.svg';
+
+
+export const homeObjOne = {
+    lightBg: false,
+    primary: true,
+    imgStart: '',
+    lightTopLine: true,
+    lightTextDesc: true,
+    buttonLabel: 'Get Started',
+    description: 'We help business owners increase their revenue. Our team of unique specialist can help you acheive your business goals',
+    headLine: 'Lead Generation Specialist for Online Businesses',
+    lightText: true,
+    topLine: 'Marketing Agency',
+    img: `${home}`,
+    alt: 'Image',
+    start: ''
+}
+
+export const homeObjTwo = {
+    lightBg: true,
+    primary: false,
+    imgStart: 'start',
+    lightTopLine: false,
+    lightTextDesc: false,
+    buttonLabel: 'Get Started',
+    description: 'We help business owners increase their revenue. Our team of unique specialist can help you acheive your business goals',
+    headLine: 'Lead Generation Specialist for Online Businesses',
+    lightText: false,
+    topLine: 'Marketing Agency',
+    img: `${home2}`,
+    alt: 'Image',
+    start: 'true'
+}
+
+
+export const homeObjThree = {
+    lightBg: false,
+    primary: true,
+    imgStart: 'start',
+    lightTopLine: true,
+    lightTextDesc: true,
+    buttonLabel: 'Get Started',
+    description: 'We help business owners increase their revenue. Our team of unique specialist can help you acheive your business goals',
+    headLine: 'Lead Generation Specialist for Online Businesses',
+    lightText: true,
+    topLine: 'Marketing Agency',
+    img: `${home3}` ,
+    alt: 'Image',
+    start: 'true'
+}
+
+export const homeObjFour = {
+    lightBg: false,
+    primary: true,
+    imgStart: '',
+    lightTopLine: true,
+    lightTextDesc: true,
+    buttonLabel: 'Get Started',
+    description: 'We help business owners increase their revenue. Our team of unique specialist can help you acheive your business goals',
+    headLine: 'Lead Generation Specialist for Online Businesses',
+    lightText: true,
+    topLine: 'Marketing Agency',
+    img: `${home4}`,
+    alt: 'Image',
+    start: ''
+}
